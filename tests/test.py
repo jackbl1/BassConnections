1332e7cb0e66da516a6efe567eddfb0204505ee5
Jack Bloomfeld <jackbloomfeld1@gmail.com>
2019-04-29 17:43:42 -0400

    Add files via upload

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os, sys
+
+path = "./data/"
+dirs = os.listdir(path)
+for file in dirs:
+    print(file)
+#df = pd.read_csv('MATRICULA_CO.csv')
+df = pd.read_csv('testbook.csv', delimiter = '|')
+col_names = ['Student ID', 'PK_COD_MATRICULA']
+newDF = pd.DataFrame(columns = col_names)
+c = 0
+for index, row in df.iterrows():
+    print(row['NUM_IDADE'])
+    if(row['NUM_IDADE'] == 8):
+        newDF.loc[c] = [row['NUM_IDADE'], row['PK_COD_MATRICULA']]
+        c+=1
+
+newDF.to_csv('output.csv')
+print(c)
+
